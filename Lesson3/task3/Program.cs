e5109d419d10918f235c28963651722ab1726a47
Yakov <ermjakov@gmail.com>
2024-04-17 22:35:24 +0300

    add Lesson3: task3

---
+++ Lesson3/task3/Program.cs
@@ -0,0 +1,33 @@
+﻿void ZeroEvenElements(int[] arr)  //  функция заполнения массива
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)  // функция вывода массива
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} "); // не индекс массива
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+ZeroEvenElements(array);
+PrintArray(array);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+
+Console.WriteLine();
+int i = 0;
+while (i < array.Length)
+{
+    Console.Write(array[i]+" ");
+    i++;
+}
